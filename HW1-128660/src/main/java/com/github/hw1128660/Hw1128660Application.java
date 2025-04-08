f9fd5570658e26154bea717f8e9251cb8f918f1a
Victor Milhomem <victormllosada@gmail.com>
2025-04-08 20:15:54 +0100

    First project commit

---
+++ HW1-128660/src/main/java/com/github/hw1128660/Hw1128660Application.java
@@ -0,0 +1,13 @@
+package com.github.hw1128660;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw1128660Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw1128660Application.class, args);
+    }
+
+}
---
+++ HW1-128660/src/test/java/com/github/hw1128660/Hw1128660ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hw1128660;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw1128660ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
