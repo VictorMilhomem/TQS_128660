eb91e0653ec4f53794266938919c54b9f25205d0
Victor Milhomem <victormllosada@gmail.com>
2025-04-09 23:22:41 +0100

    docs

--- HW1-128660/src/main/resources/test-restaurants.sql
+++ HW1-128660/src/main/resources/test-restaurants.sql
@@ -1,2 +1,11 @@
+DROP TABLE IF EXISTS meal;
+DROP TABLE IF EXISTS restaurant;
+
+CREATE TABLE restaurant (
+                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                            name VARCHAR(255) NOT NULL,
+                            location VARCHAR(255) NOT NULL
+);
+
 INSERT INTO restaurant (name, location) VALUES ('Campus Cafe', 'Building A');
 INSERT INTO restaurant (name, location) VALUES ('University Diner', 'Building B');
